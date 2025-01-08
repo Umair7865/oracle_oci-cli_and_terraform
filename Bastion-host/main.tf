957916468d80ab0b6bf9e20019d81f0dcdb106d1
Syed Umair Ali Gillani <87699636+Umair-Gillani@users.noreply.github.com>
2025-01-08 18:30:37 +0500

    Update main.tf

--- Bastion-host/main.tf
+++ Bastion-host/main.tf
@@ -6,14 +6,14 @@ provider "oci" {
   region           = var.region
 }
 
-terraform {
-  required_providers {
-    oci = {
-      source  = "oracle/oci"
-      version = "~> 6.21.0"
-    }
-  }
-}
+#terraform {
+#  required_providers {
+#    oci = {
+#      source  = "oracle/oci"
+#      version = "~> 6.21.0"
+#    }
+#  }
+#}
 
 
 
